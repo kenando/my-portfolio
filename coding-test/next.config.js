20104d88bdaabbb2e7a14d4d90c2856f124de18b
Ken Ando <d21sm006@yamanashi.ac.jp>
2023-09-15 02:56:53 +0900

    環境変数の設定

---
+++ coding-test/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.ID_PROVIDER_APIKEY,
+    NEXT_PUBLIC_API_URL: process.env.ID_PROVIDER_AUTHDOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGEING_SENDER_ID: process.env.MESSAGEING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+  },
+};
