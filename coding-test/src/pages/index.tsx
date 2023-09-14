481034305e26cc18b10c29f2cead1cf29a734e28
Ken Ando <d21sm006@yamanashi.ac.jp>
2023-09-15 01:49:08 +0900

    homeの実装

---
+++ coding-test/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignUpForm from '../pages/signup/index'
+
+export const Home = () => {
+  return (
+    <>
+      <div>
+        <SignUpForm />
+      </div>
+    </>
+  )
+}
+
+export default Home
